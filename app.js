737d6e178d016935714ebb8d4a0545a8a2dc89c1
anuragvathalurin <anuragvathalurin@gmail.com>
2022-01-25 18:09:47 -0500

    Added yargs command line parameters

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
 //Customize yargs version
-yargs.version("1.1.0");
+//yargs.version("1.1.0");
 
 //Create add command
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
   },
 });
 
@@ -23,4 +36,22 @@ yargs.command({
   },
 });
 
-console.log(yargs.argv);
+//Create list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("Listing all notes!");
+  },
+});
+
+//Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note!");
+  },
+});
+
+yargs.parse();
