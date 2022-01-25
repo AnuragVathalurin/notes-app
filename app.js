01f99f2660b2163c409e62a0bf2850adeb05d504
asv9mn <asv9mn@virginia.edu>
2022-01-25 02:02:24 -0500

    Init

---
+++ app.js
@@ -0,0 +1,26 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
+
+//Customize yargs version
+yargs.version("1.1.0");
+
+//Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+
+//Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing a note!");
+  },
+});
+
+console.log(yargs.argv);
